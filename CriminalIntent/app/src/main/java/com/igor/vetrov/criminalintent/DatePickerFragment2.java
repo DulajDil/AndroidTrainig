fcb0c75b48021ff5deae1837f6613d8dc902534a
Igor Golubenkov <i.golubenkov@mail.ru>
2017-04-24 07:38:53 +0300

    working two job

---
+++ CriminalIntent/app/src/main/java/com/igor/vetrov/criminalintent/DatePickerFragment2.java
@@ -0,0 +1,15 @@
+package com.igor.vetrov.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DatePickerFragment2 extends DialogFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+    }
+}
