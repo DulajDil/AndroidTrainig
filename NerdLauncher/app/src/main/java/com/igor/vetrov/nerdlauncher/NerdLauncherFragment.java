83639079bce25613cb13e5768d65146e2912d3e9
DESKTOP-K4L8N48\Игорь Тест <iogolubenkov@8bitov.com>
2017-07-12 18:35:43 +0300

    show list activity to name and icon, with intent launcher main

--- NerdLauncher/app/src/main/java/com/igor/vetrov/nerdlauncher/NerdLauncherFragment.java
+++ NerdLauncher/app/src/main/java/com/igor/vetrov/nerdlauncher/NerdLauncherFragment.java
@@ -1,6 +1,7 @@
 package com.igor.vetrov.nerdlauncher;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
@@ -11,7 +12,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 
@@ -29,7 +35,7 @@ public class NerdLauncherFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup conteiner, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_nerd_launcher, conteiner, false);
 
-        mRecyclerView = (RecyclerView) v.findViewById(R.id.fragment_nerd_launcher_recycler_view);
+        mRecyclerView = v.findViewById(R.id.fragment_nerd_launcher_recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         setupAdapter();
         return v;
@@ -41,6 +47,69 @@ public class NerdLauncherFragment extends Fragment {
 
         PackageManager pm = getActivity().getPackageManager();
         List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
+
+        Collections.sort(activities, (a, b) -> {
+            PackageManager pm1 = getActivity().getPackageManager();
+            return String.CASE_INSENSITIVE_ORDER.compare(a.loadLabel(pm1).toString(), b.loadLabel(pm1).toString());
+        });
         Log.i(TAG, "Found " + activities.size() + " activities.");
+        mRecyclerView.setAdapter(new ActivityAdapter(activities));
+    }
+
+    private class ActivityHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        private ResolveInfo mResolveInfo;
+        private TextView mNameTextView;
+        private ImageView mIconImageView;
+
+        public ActivityHolder(View itemView) {
+            super(itemView);
+            mNameTextView = (TextView) itemView.findViewById(R.id.activity_text);
+            mIconImageView = (ImageView) itemView.findViewById(R.id.activity_icon);
+        }
+
+        public void bindActivity(ResolveInfo resolveInfo) {
+            mResolveInfo = resolveInfo;
+            PackageManager pm = getActivity().getPackageManager();
+            String appName = mResolveInfo.loadLabel(pm).toString();
+            mNameTextView.setText(appName);
+            mIconImageView.setImageDrawable(mResolveInfo.loadIcon(pm));
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            ActivityInfo actInfo = mResolveInfo.activityInfo;
+            Intent i = new Intent(Intent.ACTION_MAIN).setClassName(actInfo.applicationInfo.packageName, actInfo.name);
+
+            startActivity(i);
+        }
+    }
+
+    private class ActivityAdapter extends RecyclerView.Adapter<ActivityHolder> {
+
+        private final List<ResolveInfo> mActivities;
+
+        public ActivityAdapter(List<ResolveInfo> activities) {
+            mActivities = activities;
+        }
+
+        @Override
+        public ActivityHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater.inflate(R.layout.list_item_activity, parent, false);
+            return new ActivityHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ActivityHolder holder, int position) {
+            ResolveInfo resolveInfo = mActivities.get(position);
+            holder.bindActivity(resolveInfo);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mActivities.size();
+        }
     }
 }
