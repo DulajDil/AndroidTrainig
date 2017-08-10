7a90e2538e5fe32ae05bdb6d0031a5fa77df5326
DESKTOP-K4L8N48\Игорь Тест <iogolubenkov@8bitov.com>
2017-08-10 18:36:07 +0300

    ref 3

--- PhotoGallery/app/src/main/java/com/igor/vetrov/photogallery/FlickrFetchr2.java
+++ PhotoGallery/app/src/main/java/com/igor/vetrov/photogallery/FlickrFetchr2.java
@@ -1,18 +1,6 @@
 package com.igor.vetrov.photogallery;
 
 
-import com.igor.vetrov.photogallery.model.GalleryItem;
-import com.igor.vetrov.photogallery.model.ResponsePhotogallery;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import okhttp3.OkHttpClient;
-import retrofit2.Call;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
 public class FlickrFetchr2 {
 
     public static final String API_KEY = "d906a0a1c11d014f5e124352d8f926b7";
--- PhotoGallery/app/src/main/java/com/igor/vetrov/photogallery/PhotoGalleryClient.java
+++ PhotoGallery/app/src/main/java/com/igor/vetrov/photogallery/PhotoGalleryClient.java
@@ -1,10 +1,8 @@
 package com.igor.vetrov.photogallery;
 
 
-import com.igor.vetrov.photogallery.model.GalleryItem;
 import com.igor.vetrov.photogallery.model.ResponsePhotogallery;
 
-import java.util.List;
 import java.util.Map;
 
 import retrofit2.Call;
